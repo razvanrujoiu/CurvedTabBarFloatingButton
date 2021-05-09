a8b26f89bf5b9e2282e7b6f8a3b49b8277a10389
Razvan Rujoiu <rjrazvan@icloud.com>
2021-05-09 12:58:11 +0300

    Initial Commit

---
+++ CurvedTabBarFloatingButton/CurvedTabBarFloatingButton.h
@@ -0,0 +1,18 @@
+//
+//  CurvedTabBarFloatingButton.h
+//  CurvedTabBarFloatingButton
+//
+//  Created by Razvan Rujoiu on 09.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CurvedTabBarFloatingButton.
+FOUNDATION_EXPORT double CurvedTabBarFloatingButtonVersionNumber;
+
+//! Project version string for CurvedTabBarFloatingButton.
+FOUNDATION_EXPORT const unsigned char CurvedTabBarFloatingButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CurvedTabBarFloatingButton/PublicHeader.h>
+
+
